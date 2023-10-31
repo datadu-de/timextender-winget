e15559dd4df4b3f38d985fbdcb25f9dbf75c1cbf
datadu-de <frank.wagner.1983@gmail.com>
2023-10-31 16:22:22 +0000

    readability

--- update-timextender.ps1
+++ update-timextender.ps1
@@ -1,1 +1,3 @@
-wingetcreate update TimeXtender.TimeXtender -u "https://ftp-2.hostedftp.com/~timextender/TimeXtender+SaaS/TimeXtender.zip" -i
+wingetcreate update TimeXtender.TimeXtender `
+    -u "https://ftp-2.hostedftp.com/~timextender/TimeXtender+SaaS/TimeXtender.zip" `
+    --interactive
